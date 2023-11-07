fc008a7a159fad750425ca19bb73e7ab5d02ae32
kelvin <kelvinokoye413@gmail.com>
2023-11-07 02:32:32 +0100

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-def replace_in_list(my_list, idx, element):
+def element_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
-        return my_list
-    my_list[idx] = element
-    return my_list
+        return
+    return my_list[idx]
