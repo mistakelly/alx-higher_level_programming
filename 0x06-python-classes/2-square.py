2add44c9fb00e923e302bfc6c7d5d8f4b6271b3b
kelvin <kelvinokoye413@gmail.com>
2023-12-01 14:13:08 +0100

    revision

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,11 +11,9 @@ class Square:
         the init function that gets invoked when a new instance is created
         :param size:
         """
-        try:
-            if not isinstance(size, int):
-                raise TypeError("size must be an integer")
-            elif size < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = size
 
-
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
