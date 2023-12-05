1b172ed9e56b61f4fdd27c389b914202df6208f2
kelvin <kelvinokoye413@gmail.com>
2023-12-05 22:11:26 +0100

    fixed the pull issue

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""Defines read file module """
+"""
+Contains the read_file function
+"""
 
 
 def read_file(filename=""):
-    """
-    read_file function
-    reads a text file (UTF8) and prints it result to stdout
-    """
-    with open(filename, mode='r', encoding='utf-8') as f:
-        print(f.read(), end='')
+    """""reads a text file(UTF8) and prints it to stdout"""
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
