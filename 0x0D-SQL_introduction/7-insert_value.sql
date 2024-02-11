330aec985678db13456f3c4fa3ad9f4791165e1d
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-11 02:51:10 +0100

    added backticks

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,6 @@
 -- script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
+-- id == 89
+-- name = Best School
 
 INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best school");
 
