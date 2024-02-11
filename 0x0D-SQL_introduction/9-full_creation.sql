8aff5011c6a35bbd8e4ede83593e67e5466fae86
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-11 03:30:43 +0100

    create and insert second_table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -8,9 +8,9 @@ CREATE TABLE IF NOT EXISTS second_table(
 );
 
 
-INSERT INTO second_table(id, name, score) 
-VALUES (1,"John", 10),
-	 (2, "Alex", 3),
-	 (3, "Bob", 14),
-	 (4, "George, 8);
+--INSERT INTO second_table(id, name, score) 
+--VALUES (1,"John", 10),
+	-- (2, "Alex", 3),
+	 --(3, "Bob", 14),
+	 --(4, "George, 8);
 
