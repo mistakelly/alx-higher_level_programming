611af191dbc595e1d4af7796d306d2dd05229db9
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-11 02:53:03 +0100

    added backticks

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -2,5 +2,6 @@
 -- id == 89
 -- name = Best School
 
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best school");
+--INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best school");
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
 
