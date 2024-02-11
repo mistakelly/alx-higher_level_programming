5943cf5f2b612ea67d992a8b0c28f4f4dee18bd7
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-11 03:34:33 +0100

    updated the format of the querry

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -7,6 +7,13 @@ CREATE TABLE IF NOT EXISTS second_table(
 	score INT
 );
 
+INSERT INTO second_table(id, name, score) VALUES (1, "John", 10);
+INSERT INTO second_table(id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO second_table(id, name, score) VALUES (3, "Bob", 14);
+INSERT INTO second_table(id, name, score) VALUES (4, "George", 8);
+
+
+
 
 --INSERT INTO second_table(id, name, score) 
 --VALUES (1,"John", 10),
