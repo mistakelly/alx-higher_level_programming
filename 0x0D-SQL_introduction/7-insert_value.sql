93065a7c227f9cfb2218d42b415461da92345006
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-11 02:56:31 +0100

    fixing error

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,7 +1,3 @@
--- script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
--- id == 89
--- name = Best School
-
---INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best school");
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
+-- Inserts a new row into the table first_table.
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School")
 
