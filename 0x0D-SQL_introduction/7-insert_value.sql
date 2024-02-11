986c278117545a2710a1724be1776997ce8dae8b
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-11 03:01:37 +0100

    capitals fixed

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,7 @@
--- Inserts a new row into the table first_table.
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School")
+-- a script that inserts row
 
+INSERT INTO first_table (id, name) VALUES (89, "Best School");
+
+
+-- Inserts a new row into the table first_table.
+--INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School")
