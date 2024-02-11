b73292f326e3ad8e39c356a9966edd2381474dfb
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-11 03:47:20 +0100

    list contents of second database

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,4 @@
 -- Write a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
 
-SELECT * FROM second_table WHERE score = true;
-SELECT * FROM second_table WHERE name = true;
+SELECT * FROM second_table WHERE score > 0;
+SELECT * FROM second_table WHERE name IS NOT NULL AND name <> '';
