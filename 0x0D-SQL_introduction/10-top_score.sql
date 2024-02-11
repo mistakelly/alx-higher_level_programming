0a33e47f2cd9e7e4595d201e502031ffdfe3710c
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-11 03:52:29 +0100

    list by reversal

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,6 @@
 -- Write a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
 
-SELECT * FROM second_table WHERE score > 0;
-SELECT * FROM second_table WHERE name IS NOT NULL AND name <> '';
+SELECT `score`, `name`
+FROM second_table
+ORDER BY `score`
+
