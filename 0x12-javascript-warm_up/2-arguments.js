9de8b7cfb23af729ea97804be2148b7447e9143a
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-12 18:46:49 +0100

    task 2 done

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const args = process.argv
+
+const usr_input = args.length
+
+if (usr_input < 3){
+  console.log('No argument')
+}else if (usr_input === 3){
+  console.log('Argument found')
+}
+else{
+  console.log('Argument found')
+}
