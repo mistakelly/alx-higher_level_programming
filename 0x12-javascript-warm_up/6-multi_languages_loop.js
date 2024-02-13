96dcdf46a69bfabd78e0f2f7209907cb79efcc4d
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-13 04:03:31 +0100

    task6 done

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < languages.length; i++) {
   console.log(languages[i]);
 }
