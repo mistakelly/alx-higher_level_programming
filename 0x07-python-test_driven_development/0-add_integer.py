8c84a2d4b09da464ed5f96352586465a9d70eb23
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-13 16:31:31 +0100

    convert to int if argument is float

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,7 @@
-#!/usr/bin/python3
 """a script that adds two integers
 and returns the output, exeception is raised when
 when a the user passes a non int/float value.
 """
-
 
 def add_integer(a, b=98):
     """
@@ -17,12 +15,17 @@ def add_integer(a, b=98):
     """
     if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
         raise TypeError("a must be an integer or b must be an integer")
+    
+    # convert to int if argument is float
+    if isinstance(a, float) or isinstance(b, float):
+        a = int(a)
+        b = int(b)
     sum = a + b
     return sum
 
 
 try:
-    total = add_integer(4.8, 4.4)
+    total = add_integer(4.9, 4.9)
     print(total)
 except TypeError as te:
     print(te)
