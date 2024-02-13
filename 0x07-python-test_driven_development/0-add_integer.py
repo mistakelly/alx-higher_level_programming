15b66e3cb49636c2ce037676c50b51dac3478cc2
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-13 17:22:41 +0100

    added float

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -18,9 +18,9 @@ def add_integer(a, b=98):
     if isinstance(a, float) or isinstance(b, float):
         a = int(a)
         b = int(b)
-    if not isinstance(a, (int)):
+    if not isinstance(a, (int,float)):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int)):
+    if not isinstance(b, (int, float)):
          raise TypeError("b must be an integer")
 
     sum = a + b
