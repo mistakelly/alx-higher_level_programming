58be3edd385159ed0edbe7f0d02f30dde6dde42b
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-13 03:56:04 +0100

    updated task5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,7 @@
 const array = process.argv;
 const convertedNum = parseInt(array[2], 10);
-console.log(`My number: ${convertedNum}`);
 if (isNaN(convertedNum)) {
   console.log('Not a number');
+} else {
+  console.log(`My number: ${convertedNum}`);
 }
