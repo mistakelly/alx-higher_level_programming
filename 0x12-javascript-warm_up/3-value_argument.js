79621f274c0af0c0b510a6c02039c2109b2188dd
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-13 02:59:04 +0100

    done with task3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,6 +3,5 @@ if (process.argv[2] === undefined)
 {
     console.log('No argument');
 }else{
-    firstArg = process.argv.slice(2)
-    console.log(firstArg)
+    console.log( process.argv.slice(2));
 }
