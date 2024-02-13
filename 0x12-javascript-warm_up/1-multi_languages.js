8c9d66fbc7287f4608db103a862b4ba2d7084c76
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-13 01:32:41 +0100

    rm newline

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-
-console.log('C is fun')
-console.log('Python is cool')
-console.log ('JavaScript is amazing')
+console.log('C is fun');
+console.log('Python is cool');
+console.log ('JavaScript is amazing');
 
