d6cac7f9e3ebb8f3e1087aed3305f398de20dcc8
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-14 11:18:55 +0100

    updated task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,4 +1,13 @@
 #!/usr/bin/node
-function add (a, b) {
-  console.log(a + b);
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+function add (num1, num2) {
+  const arg1Number = parseInt(num1, 10);
+  const arg2Number = parseInt(num2, 10);
+  const sum = arg1Number + arg2Number;
+
+  return sum;
 }
+const result = add(arg1, arg2);
+console.log(result);
