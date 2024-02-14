3c99cc5d39faebf1709baf5df9f84019b2d47e2c
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-14 14:44:13 +0100

    initialize constructor

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/*
+Write a class Rectangle that defines a rectangle:
+
+You must use the class notation for defining your class
+The constructor must take 2 arguments w and h
+Initialize the instance attribute width with the value of w
+Initialize the instance attribute height with the value of h
+*/
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
