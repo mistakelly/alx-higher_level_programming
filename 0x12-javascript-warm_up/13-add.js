86ae32265d124b15b8e94cd4edde608900fedb22
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-14 12:31:24 +0100

    updated task 13

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,4 +1,10 @@
-#!/usr/bin/node
-exports.add = function (a, b) {
-  return (a + b);
-};
+/* Write a function that returns the addition of 2 integers.
+
+The function must be visible from outside
+The name of the function must be add
+You are not allowed to use var
+*/
+
+function add (num1, num2) {
+  return num1 + num2;
+}
