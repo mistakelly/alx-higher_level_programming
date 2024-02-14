515ae21cf958e852b51ada5095c2ec9d7d16650e
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-14 11:12:07 +0100

    updated task9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,13 +1,4 @@
 #!/usr/bin/node
-const args = process.argv;
-const A = parseInt(args[2]);
-const B = parseInt(args[3]);
-if (typeof A === 'number' && typeof B === 'number') {
-  console.log(add(A, B));
-} else {
-  console.log('NaN');
-}
-
 function add (a, b) {
-  return (a + b);
+  console.log(a + b);
 }
