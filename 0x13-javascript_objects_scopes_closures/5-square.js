95a572cb066623cfdd00803b99996188a0812fa7
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-14 17:42:06 +0100

    updated task 5

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -7,36 +7,7 @@ The constructor must take 1 argument: size
 The constructor of Rectangle must be called (by using super())
 
  */
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  rotate () {
-    const tmp = this.width;
-    this.width = this.height;
-    this.height = tmp;
-  }
-
-  double () {
-    this.height *= 2;
-    this.width *= 2;
-  }
-
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      let pattern = '';
-      for (let j = 0; j < this.width; j++) {
-        pattern += 'X';
-      }
-      console.log(pattern);
-    }
-  }
-}
-
+const Rectangle = require('./4-rectangle')
 class Square extends Rectangle {
   constructor (size) {
     super(size, size); // Pass size for both width and height
