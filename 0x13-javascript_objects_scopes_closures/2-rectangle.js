b56a56b78b5ede64b91a43840c08dc73103b57f3
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-14 15:10:12 +0100

    made executable

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -8,8 +8,3 @@ module.exports = class Rectangle {
     }
   }
 };
-
-// let r1 = new Rectangle(-1, -1);
-// console.log(r1);
-// console.log(r1.width); // This will be undefined because the constructor didn't initialize the properties properly
-// console.log(r1.height); // This will be undefined for the same reason
