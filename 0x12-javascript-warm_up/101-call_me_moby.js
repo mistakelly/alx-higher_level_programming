61dfb460b7ec313964930a9114ec7987383d473a
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-14 13:16:38 +0100

    task 101 done

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-exports.callMeMoby = function (x, theFunction){
-    for (let i = 0; i < x; i++) {
-       theFunction(x)
-    }
-}
-
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction(x);
+  }
+};
