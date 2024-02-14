8377b5fed81e1084b07678bd896ec99d3620b0dc
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-14 16:02:00 +0100

    added double and rotate method

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,63 @@
+#!/usr/bin/node
+/*
+Write a class Rectangle that defines a rectangle:
+
+You must use the class notation for defining your class
+The constructor must take 2 arguments: w and h
+Initialize the instance attribute width with the value of w
+Initialize the instance attribute height with the value of h
+If w or h is equal to 0 or not a positive integer, create an empty object
+Create an instance method called print() that prints the rectangle using the character X
+Create an instance method called rotate() that exchanges the width and the height of the rectangle
+Create an instance method called double() that multiples the width and the height of the rectangle by 2
+*/
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let pattern = '';
+      for (let j = 0; j < this.width; j++) {
+        pattern += 'X';
+      }
+      console.log(pattern);
+    }
+  }
+}
+
+// module.exports = Rectangle;
+// r1 = new Rectangle(4,2)
+// console.log(r1)
+// r1.rotate()
+// console.log(r1)
+// r1.double()
+// console.log(r1)
+// r1.print()
+
+const r1 = new Rectangle(2, 3);
+console.log('Normal:');
+r1.print();
+
+console.log('Double:');
+r1.double();
+r1.print();
+
+console.log('Rotate:');
+r1.rotate();
+r1.print();
