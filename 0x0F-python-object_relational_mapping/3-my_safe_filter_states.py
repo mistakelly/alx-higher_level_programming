f95d98cbc37aea551ba5ce55c85b9e783182e23c
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-18 14:40:24 +0100

    prevented sql injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -24,32 +24,43 @@ Results must be sorted in ascending order by states.id
 Results must be displayed as they are in the example below
 Your code should not be executed when imported
 """
-if __name__ == '__main__':
-    # receive command line arguments
-    import MySQLdb as mysql
-    from sys import argv
-    username, password, database, searched = argv[1], argv[2], argv[3], argv[4]
 
-    # establish connection
+import MySQLdb as mysql
+from sys import argv
 
+
+def connection(connect, username, password, database, search, prt):
+    """
+        lists all row in states table
+        connect:port,
+        username: username of the user,
+        password: password of the database,
+        database: the database to use,
+        prt: port to connect.
+    """
     mydb = mysql.connect(
-        host='localhost',
+        host=connect,
         user=username,
         passwd=password,
         db=database,
-        port=3306
+        port=prt
     )
-    # create a cursor
+
+    # create cursor
     cursor = mydb.cursor()
-    command = "SELECT * FROM `states` WHERE name LIKE BINARY %s"
+    command = "SELECT * FROM states WHERE name LIKE BINARY %s"
 
-    cursor.execute(command, (searched,))
-    # fetch all data
+    cursor.execute(command, (search, ))
+
     rows = cursor.fetchall()
-
     for row in rows:
         print(row)
 
     # close connections
     cursor.close()
-    mydb.close()
+    return mydb
+
+
+if __name__ == '__main__':
+    mydbase = connection('localhost', argv[1], argv[2], argv[3], argv[4], 3306)
+    mydbase.close()
