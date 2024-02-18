0f7422bb4e5d644f3932058c539d3097b9f29cea
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-18 12:44:24 +0100

    added an if name == main block

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,33 +2,33 @@
 """
 prevent sql injection
 """
+if __name__ == '__main__':
+    # receive command line arguments
+    import MySQLdb as mysql
+    from sys import argv
+    username, password, database, searched = argv[1], argv[2], argv[3], argv[4]
 
-# receive command line arguments
-import MySQLdb as mysql
-from sys import argv
-username, password, database, searched = argv[1], argv[2], argv[3], argv[4]
+    # establish connection
 
-# establish connection
-
-mydb = mysql.connect(
-    host='localhost',
-    user=username,
-    passwd=password,
-    db=database,
-    port=3306
-)
+    mydb = mysql.connect(
+        host='localhost',
+        user=username,
+        passwd=password,
+        db=database,
+        port=3306
+    )
 
 
-# create a cursor
-cursor = mydb.cursor()
+    # create a cursor
+    cursor = mydb.cursor()
 
-cursor.execute('SELECT * FROM states WHERE name LIKE BINARY %s', (searched,))
-# fetch all data
-rows = cursor.fetchall()
+    cursor.execute('SELECT * FROM states WHERE name LIKE BINARY %s', (searched,))
+    # fetch all data
+    rows = cursor.fetchall()
 
-for row in rows:
-    print(row)
+    for row in rows:
+        print(row)
 
-# close connections
-cursor.close()
-mydb.close()
+    # close connections
+    cursor.close()
+    mydb.close()
