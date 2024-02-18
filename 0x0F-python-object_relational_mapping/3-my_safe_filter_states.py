e19186bb7998f257cb32421822938f2c9d8a41cd
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-18 02:03:04 +0100

    updated task3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,28 +1,6 @@
 #!/usr/bin/python3
 """
-Wait, do you remember the previous task? Did you test
-"Arizona'; TRUNCATE TABLE states ;
-SELECT * FROM states WHERE name = '" as an input?
-
-guillaume@ubuntu:~/0x0F$ ./2-my_filter_states.py
-root root hbtn_0e_0_usa "Arizona';
-TRUNCATE TABLE states ; SELECT * FROM states WHERE name = '"
-(2, 'Arizona')
-guillaume@ubuntu:~/0x0F$ ./0-select_states.py root root hbtn_0e_0_usa
-guillaume@ubuntu:~/0x0F$
-What? Empty?
-
-Yes, it’s an SQL injection to delete all records of a table…
-Once again, write a script that takes in arguments and displays
-all values in the states table of hbtn_0e_0_usa where name matches
-the argument. But this time, write one that is safe from MySQL injections!
-Your script should take 4 arguments: mysql username, mysql password,
-database name and state name searched (safe from MySQL injection)
-You must use the module MySQLdb (import MySQLdb)
-Your script should connect to a MySQL server running on localhost at port 3306
-Results must be sorted in ascending order by states.id
-Results must be displayed as they are in the example below
-Your code should not be executed when imported
+prevent sql injection
 """
 
 # receive command line arguments
@@ -43,9 +21,6 @@ mydb = mysql.connect(
 
 # create a cursor
 cursor = mydb.cursor()
-
-# command = f'SELECT * FROM states WHERE name=%s ORDER BY states.id'
-
 
 cursor.execute('SELECT * FROM states WHERE name LIKE BINARY %s', (searched,))
 # fetch all data
