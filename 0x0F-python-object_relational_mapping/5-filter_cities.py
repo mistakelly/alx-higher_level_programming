a670c2805efe7a3898b20beccf5512fa46c88338
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-18 18:42:27 +0100

    order by cities.id

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -19,14 +19,6 @@ from sys import argv
 
 
 def connection(connect, username, password, database, prt, state):
-    """
-        lists all row in states table
-        connect:port,
-        username: username of the user,
-        password: password of the database,
-        database: the database to use,
-        prt: port to connect.
-    """
     mydb = mysql.connect(
         host=connect,
         user=username,
@@ -35,23 +27,14 @@ def connection(connect, username, password, database, prt, state):
         port=prt
     )
 
-    # create cursor
     cursor = mydb.cursor()
-    query = "SELECT name FROM cities WHERE state_id =" \
-            "(SELECT id FROM states WHERE name=%s) ORDER BY cities.id ASC"
+    query = "SELECT name FROM cities WHERE state_id = (SELECT id FROM states WHERE name=%s) ORDER BY cities.id ASC"
     cursor.execute(query, (state,))
 
     rows = cursor.fetchall()
-    count = 0
     for row in rows:
-        for i in row:
-            if count < 2:
-                print(i, end=', ')
-            else:
-                print(i, end=' ')
-            count += 1
-    print()
-    # close connections
+        print(', '.join(map(str, row)))
+
     cursor.close()
     return mydb
 
@@ -59,3 +42,4 @@ def connection(connect, username, password, database, prt, state):
 if __name__ == '__main__':
     mydbase = connection('localhost', argv[1], argv[2], argv[3], 3306, argv[4])
     mydbase.close()
+
