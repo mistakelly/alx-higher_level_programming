ef80900ff5ee5038be737c4657d64c7ca2976d86
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-21 00:20:36 +0100

    defined class

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,10 +1,12 @@
 from sqlalchemy import Column, String, Integer, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
+# engin
 
 # create connection
 Base = declarative_base()
 
 
 class State(Base):
-    pass
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128))
