0c20ff3123f6335fea1d83f29bf949db9aa87a23
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-20 23:59:59 +0100

    model

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, String, Integer, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+
+# create connection
+Base = declarative_base()
+
+
+
+class State(Base):
+    pass
