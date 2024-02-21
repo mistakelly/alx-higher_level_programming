044a40d3958eb077ea9d26194bdd4ff448aea6a9
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-21 17:09:39 +0100

    changed table name

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,7 +9,7 @@ Base = declarative_base()
 
 
 class State(Base):
-    __tablename__ = 'users'
+    __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128))
 
