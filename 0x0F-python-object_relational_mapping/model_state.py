f6ea1fc931fa07962a85f72dd9c2ed9e509de9c1
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-22 02:33:00 +0100

    added metadata

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,10 +5,11 @@ table in USE hbtn_0e_6_usa;
 sqlalchemy helps us to define databases as python objects and work
 with them, so we  don't have to write raw sql codes
 """
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
+metadata = MetaData()
+Base = declarative_base(metaData=MetaData)
 
 
 class State(Base):
