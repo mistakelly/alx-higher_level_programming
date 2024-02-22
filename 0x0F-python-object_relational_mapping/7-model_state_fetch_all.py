d2b76983fd8b5b78b7c58ab1413b553c4288cad2
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-22 15:58:36 +0100

    a script that lists all states in database using sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,11 @@
+"""
+a script that lists all State objects from the database hbtn_0e_6_usa
+
+Your script should take 3 arguments: mysql username, mysql password and database name
+You must use the module SQLAlchemy
+You must import State and Base from model_state - from model_state import Base, State
+Your script should connect to a MySQL server running on localhost at port 3306
+Results must be sorted in ascending order by states.id
+The results must be displayed as they are in the example below
+Your code should not be executed when imported
+"""
