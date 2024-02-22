5e1c660a30f25fb7b32280db1deee36e81039f9f
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-22 02:34:50 +0100

    restructured

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,14 +5,19 @@ table in USE hbtn_0e_6_usa;
 sqlalchemy helps us to define databases as python objects and work
 with them, so we  don't have to write raw sql codes
 """
-from sqlalchemy import Column, String, Integer, MetaData
+from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
-metadata = MetaData()
-Base = declarative_base(metaData=MetaData)
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
 
 
 class State(Base):
+
+    """
+    Class with attributes of a state.
+    """
+
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False)
-    name = Column(String(128))
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
