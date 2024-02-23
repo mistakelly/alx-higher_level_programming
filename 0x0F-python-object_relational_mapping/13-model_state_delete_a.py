86f760be545667f4a113f7fefc61692fd66644fd
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-23 13:11:30 +0100

    deleted colums with letter a in them

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -12,3 +12,32 @@ Your script should connect to a MySQL server running
 on localhost at port 3306
 Your code should not be executed when imported
 """
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+def del_state_col(username, password, db):
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, db), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    del_states = session.query(State).filter(State.name.like('%a%')).all()
+    states = session.query(State).all()
+
+    # loop through the list update_names and drop row
+    for del_state in del_states:
+        session.delete(del_state)
+    for state in states:
+        print('{}: {}'.format(state.id, state.name))
+
+    # session.add(update_name)
+    session.commit()
+    session.close()
+
+
+if __name__ == '__main__':
+    del_state_col(argv[1], argv[2], argv[3])
