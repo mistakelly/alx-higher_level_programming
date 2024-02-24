0d69b5ad192c137140488ce519fff885fe3997a3
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-24 04:27:40 +0100

    counted recorded

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,13 @@
+-- Write a script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server.
+
+-- The result should display:
+-- the score
+-- the number of records for this score with the label number
+-- The list should be sorted by the number of records (descending)
+-- The database name will be passed as an argument to the mysql command
+
+
+SELECT score, COUNT(*) AS number
+FROM states
+GROUP BY score
+ORDER BY number DESC
