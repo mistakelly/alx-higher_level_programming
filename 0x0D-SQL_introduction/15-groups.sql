95b667d1dc52af401c9224346b0f6419f5460539
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-04 10:02:39 +0100

    group by

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -4,7 +4,6 @@
 -- the score
 -- the number of records for this score with the label number
 -- The list should be sorted by the number of records (descending)
--- The database name will be passed as an argument to the mysql command
 
 
 SELECT score, COUNT(*) AS number
