74e0acf5d482de2054ebd7e57aaea8ac447b72ad
kelvin okoye <kelvinokoye413@gmail.com>
2024-03-09 02:21:16 +0000

    state with highest tmp

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,7 +1,7 @@
 -- select state with highest temperature
 -- order by state
 
-select state, MAX(value) AS 'max_temp'
-from temperatures
+SELECT state, MAX(value) AS 'max_temp'
+FROM temperatures
 GROUP BY state
 ORDER BY state
