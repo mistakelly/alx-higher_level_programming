f9aac88b7ffd2ba512583df050472b5d337af54c
kelvin okoye <kelvinokoye413@gmail.com>
2024-03-09 02:08:47 +0000

    rm s

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,6 @@
 -- QUERRY THAT SEARCHES TEMPERATURES TABLE, CALC AVERAGE,  AND ORDER BY AVG.
 -- group by city
-SELECT city, AVG(values) as `avg_temp`
+SELECT city, AVG(value) as `avg_temp`
 FROM temperatures
 GROUP BY city
 ORDER BY avg_temp DESC;
