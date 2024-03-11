3d9c56a2ffc107fbd276ededbd5edcfcf2a4b437
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-11 18:11:26 +0100

    delete

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++
@@ -1,9 +0,0 @@
--- a script that lists all the cities of California that can be found in the database 
--- The database name will be passed as an argument of the mysql command
-
-SELECT id, name FROM cities WHERE state_id = 
-    (
-        SELECT id FROM states
-        where name = 'California'
-    )
-ORDER BY cities.id ASC;
