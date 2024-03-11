321e02de1b1d7f89aee88ba224cb916d6d2904f2
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-11 15:26:10 +0100

    create and assing select option

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,12 +3,8 @@
 -- The user_0d_2 password should be set to user_0d_2_pwd
 -- If the database hbtn_0d_2 already exists, your script should not fail
 -- If the user user_0d_2 already exists, your script should not fail
-
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
-GRANT SELECT ON hbtb_0d_2.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
-
-
-
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
