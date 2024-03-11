1e0049261c7fe2a93b1d2f98452e676685938343
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-11 15:23:23 +0100

    create and assing select option

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,7 +4,7 @@
 -- If the database hbtn_0d_2 already exists, your script should not fail
 -- If the user user_0d_2 already exists, your script should not fail
 
-CREATE DATABASE IF NOT EXISTS `hbtb_0d_2`;
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
