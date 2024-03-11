66d110b205a189bd2317237ee7fab62fc77351aa
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-11 19:08:23 +0100

    all records

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- script that lists all cities contained in the database hbtn_0d_usa.
 
-SELECT cities.id, cities.name, states.name from cities, states;
+SELECT cities.id, cities.name, states.name from cities, states ORDER BY cities.id ASC;
