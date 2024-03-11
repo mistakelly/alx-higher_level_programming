ed1e50ef3732c52c3225f5aa7bd55e30bfff999c
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-11 18:47:37 +0100

    cities relationship

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,9 +1,4 @@
 -- script that lists all the cities of California that can be found in the database hbtn_0d_usa.
 
-SELECT id, name from cities
-WHERE state_id = (
-    SELECT id FROM states
-    WHERE name = 'California'
-)
-ORDER BY cities.id ASC;
+SELECT id, name from cities WHERE state_id = ( SELECT id FROM states WHERE name = 'California') ORDER BY cities.id ASC;
 
