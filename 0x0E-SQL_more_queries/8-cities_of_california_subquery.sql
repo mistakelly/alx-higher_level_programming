9a7ebc5c2628487b64746ec4eb21e8ffe7279825
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-11 17:37:14 +0100

    subquerry

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -4,7 +4,7 @@
 -- You are not allowed to use the JOIN keyword
 -- The database name will be passed as an argument of the mysql command
 
-SELECT * FROM `cities` WHERE state_id = 
+SELECT *  FROM cities WHERE state_id = 
     (
         SELECT id FROM states
         where name = 'california'
