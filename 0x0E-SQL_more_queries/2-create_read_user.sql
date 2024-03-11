dc0ff90fa1040311adf3301eea32187fccb88e24
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-11 15:22:26 +0100

    create and assing select option

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -8,7 +8,7 @@ CREATE DATABASE IF NOT EXISTS `hbtb_0d_2`;
 
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
-GRANT SELECT ON `hbtb_0d_2` TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
+GRANT SELECT ON `hbtb_0d_2`.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
 
 
 
