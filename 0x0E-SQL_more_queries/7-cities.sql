51fa5bc43ba85ab9ccdb9923856d69c3a0003814
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-11 16:07:24 +0100

    RELATIONSHIP

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -14,5 +14,5 @@ CREATE TABLE IF NOT EXISTS `cities`(
     `id` INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
     `state_id`INT NOT NULL,
     `name` VARCHAR(256) NOT NULL,
-    FOREIGN KEY(state_id) REFERENCES(states.id)
+    FOREIGN KEY(state_id) REFERENCES(states(id))
 );
