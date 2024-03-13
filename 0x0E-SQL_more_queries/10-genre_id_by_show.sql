17dbc2215a77a0277476ad53daa2f7941dd7b8b7
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-13 01:39:11 +0100

    genre

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,23 +1,14 @@
 -- a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 
-SELECT tv_shows.title AS title, COUNT(title) AS `genre_id` FROM tv_shows
-JOIN tv_show_genres ON tv_show.id = tv_show_genres.show_id 
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC
-GROUP BY tv_shows.title;
+-- SELECT tv_shows.title AS title, COUNT(title) AS `genre_id` FROM tv_shows
+-- JOIN tv_show_genres ON tv_show.id = tv_show_genres.show_id 
+-- ORDER BY tv_shows.title, tv_show_genres.genre_id ASC
 
 
-CREATE TABLE `tv_shows` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(256) NOT NULL,
-  PRIMARY KEY (`id`)
-) 
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server
 
-CREATE TABLE `tv_show_genres` (
-  `show_id` int(11) NOT NULL,
-  `genre_id` int(11) NOT NULL,
-  KEY `show_id` (`show_id`),
-  KEY `genre_id` (`genre_id`),
-  FOREIGN KEY (`show_id`) REFERENCES `tv_shows` (`id`),
-  FOREIGN KEY (`genre_id`) REFERENCES `tv_genres` (`id`)
-) 
--
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+    INNER JOIN tv_show_genres
+    ON tv_shows.id=tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
