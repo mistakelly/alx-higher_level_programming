7dc21aa651c5e95e924048bafb513789a7ae45c1
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-13 11:57:03 +0100

    shows

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -5,6 +5,7 @@
 --You can use only one SELECT statement
 --The database name will be passed as an argument of the mysql command
 
-SELECT tv_shows.title AS `title`, IFNULL(tv_show_genres.genre_id, `NULL`) as `genre_id`
-FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id =  tv_show_genres.show_id
+SELECT tv_shows.title AS `title`, tv_show_genres.genre_id as `genre_id`
+FROM tv_shows 
+LEFT JOIN tv_show_genres ON tv_shows.id =  tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
