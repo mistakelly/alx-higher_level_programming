f2fa08ab389973ad83561135802be2b08ec04e23
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-13 01:32:40 +0100

    genre

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,6 @@
 -- a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 
-SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id COUNT(*) FROM hbtn_0d_tvshows
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id COUNT(*) FROM tv_shows
+JOIN tv_show_genres ON tv_show.id = tv_show_genres.show_id 
 ORDER BY tv_shows.title, tv_show_genres.genre_id
 GROUP BY tv_shows.title, tv_show_genres.genre_id;
