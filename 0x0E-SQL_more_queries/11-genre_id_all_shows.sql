3460c72712e0138e14b7c431377d58918d0effa1
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-13 01:48:19 +0100

    NULL

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,10 @@
 -- shows contained in the database hbtn_0d_tvshows.
 
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+--Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+--You can use only one SELECT statement
+--The database name will be passed as an argument of the mysql command
+
 SELECT tv_shows.title AS `title`, tv_show_genres.genre_id from tv_shows
 INNER JOIN tv_show_genre_id ON tv_shows.id = tv_show_genres.show_id
 CASE 
