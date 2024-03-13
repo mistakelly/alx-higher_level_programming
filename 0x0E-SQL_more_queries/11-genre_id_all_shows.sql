8bd0f21bb19437f28f1c04782c22f77660728802
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-13 01:46:46 +0100

    NULL

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,9 @@
+-- shows contained in the database hbtn_0d_tvshows.
+
+SELECT tv_shows.title AS `title`, tv_show_genres.genre_id from tv_shows
+INNER JOIN tv_show_genre_id ON tv_shows.id = tv_show_genres.show_id
+CASE 
+    WHEN tv_show_genres.show_id  IS NULL THEN 'NULL'
+    ELSE  
+END
+ORDER BY tv_shows.tiltle, tv_show_genres.genre_id;
