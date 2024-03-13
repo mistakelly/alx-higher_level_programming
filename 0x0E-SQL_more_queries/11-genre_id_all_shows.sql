a2b0d45711b15896b0888275810e1af16aecfa1c
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-13 12:14:17 +0100

    tvshows contained

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -5,7 +5,8 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 
-SELECT tv_shows.title AS `title`, tv_show_genres.genre_id AS `genre` FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+SELECT tv_shows.title AS `title`, tv_show_genres.genre_id AS `genre`
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
-
