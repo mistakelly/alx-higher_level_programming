9adf4cdb6b420e9b632b1ab7b150ce35ae5e2708
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-13 01:37:11 +0100

    genre

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,7 +2,7 @@
 
 SELECT tv_shows.title AS title, COUNT(title) AS `genre_id` FROM tv_shows
 JOIN tv_show_genres ON tv_show.id = tv_show_genres.show_id 
-ORDER BY tv_shows.title, tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC
 GROUP BY tv_shows.title;
 
 
