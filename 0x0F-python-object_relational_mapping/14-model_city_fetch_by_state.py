886db981c5f7d4076df7190bd17b78e34a8412d2
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-17 18:41:54 +0100

    pycodestyle

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:33066/{}"
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True, echo=True)
-    # Base.metadata.create_all(engine)
+    Base.metadata.create_all(engine)
     # create session
     Session = sessionmaker(bind=engine)
 
