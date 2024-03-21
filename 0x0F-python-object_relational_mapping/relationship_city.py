a61ba8175a0db430e8e9642686db9834dc99efb0
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-21 04:33:26 +0100

    relationship city

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -17,6 +17,7 @@ You must use the module SQLAlchemy
 from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
+from relationship_state import State
 
 
 class City(Base):
@@ -28,8 +29,6 @@ class City(Base):
                 primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('State.id'), nullable=False)
-    state = relationship("State", back_populates="cities")
-
 
 
 
