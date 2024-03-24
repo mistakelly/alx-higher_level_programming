62d3ebad13a431dc242e7cee983942f5f50d0c01
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-24 12:52:41 +0100

    add city and state records

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -3,8 +3,9 @@
     the code should not execute if imported
 """
 from sys import argv
-from model_state import Base, State
-from model_city import City
+from relationship_state import Base
+from relationship_city import City
+from relationship_state import State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
@@ -12,13 +13,13 @@ if __name__ == '__main__':
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:33066/{}"
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
     # create session
     Session = sessionmaker(bind=engine)
 
     session = Session()
 
-    all_data = session.query(State.name, City.id, City.name)\
-        .filter(State.id == City.state_id)
-
-    for state_name, city_id, city_name in all_data:
-        print("{}: ({}) {}".format(state_name, city_id, city_name))
+    session.add(State(name='California'))
+    session.add(City(name='San Francisco', state_id=1))
+    session.commit()
