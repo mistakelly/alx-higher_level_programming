166ded52e09b061d888b85f0c5a88c1100b0e790
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-24 13:03:14 +0100

    task100

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 """
-Write a script that creates the State “California” with the City “San Francisco”
-from the database hbtn_0e_100_usa: (100-relationship_states_cities.py)
-
-Your script should take 3 arguments: mysql username, mysql password and database name
-You must use the module SQLAlchemy
-Your script should connect to a MySQL server running on localhost at port 3306
-You must use the cities relationship for all State objects
+All states via SQLAlchemy
 """
 from sys import argv
 from relationship_state import Base, State
@@ -20,11 +14,8 @@ if __name__ == "__main__":
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-
-
     session = Session(engine)
     new_state = State(name='California')
-
 
     new_city = City(name='San Francisco')
     new_state.cities.append(new_city)
