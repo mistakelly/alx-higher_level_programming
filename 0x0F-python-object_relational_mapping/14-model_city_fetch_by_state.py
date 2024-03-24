60441bcf60235acf5d686a1d1d377bbc8750a1d5
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-24 13:02:07 +0100

    task14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-"""select city and also select states (sort of join)
-    the code should not execute if imported
-"""
+'''
+a script that lists all State objects
+'''
+
+
 from sys import argv
-from relationship_state import Base
-from relationship_city import City
-from relationship_state import State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import City
 
-if __name__ == '__main__':
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:33066/{}"
-                           .format(argv[1], argv[2], argv[3]),
-                           pool_pre_ping=True)
-
+if __name__ == "__main__":
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                        argv[2],
+                                                        argv[3]))
     Base.metadata.create_all(engine)
-    # create session
     Session = sessionmaker(bind=engine)
-
     session = Session()
-
-    session.add(State(name='California'))
-    session.add(City(name='San Francisco', state_id=1))
-    session.commit()
+    c = session.query(City, State).filter(City.state_id == State.id)\
+        .order_by(City.id).all()
+    for cty, st in c:
+        print("{}: ({}) {}".format(st.name, cty.id, cty.name))
+    session.close()
